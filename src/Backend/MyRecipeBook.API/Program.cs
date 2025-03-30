2bbdd89f20acfbd352989b1cc51eca29b5897927
Vitor Santos <vitorspiri1@gmail.com>
2025-03-30 18:00:38 -0300

    feat: creating middleware to get culture info

--- src/Backend/MyRecipeBook.API/Program.cs
+++ src/Backend/MyRecipeBook.API/Program.cs
@@ -1,3 +1,4 @@
+using MyRecipeBook.Application.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +16,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
