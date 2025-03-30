823e53a00ef632b4566d390a8ee323ebea2d63a9
Vitor Santos <vitorspiri1@gmail.com>
2025-03-30 18:47:33 -0300

    feat: adding exceptions base files

---
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace MyRecipeBook.Exceptions.ExceptionsBase;
+
+public class ErrorOnValidationException : MyRecipeBookException
+{
+    IList<string> ErrorMessages { get; set; }
+
+    public ErrorOnValidationException(IList<string> errorMessages)
+    {
+        ErrorMessages = errorMessages;
+    }
+}
