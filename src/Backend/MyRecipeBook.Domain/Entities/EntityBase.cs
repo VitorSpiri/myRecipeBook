11a510f869d30e84267c64b9616e6446821f00e4
Vitor Santos <vitorspiri1@gmail.com>
2025-04-06 11:09:05 -0300

    feat: adding entityBase class

---
+++ src/Backend/MyRecipeBook.Domain/Entities/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Domain.Entities
+{
+    public class EntityBase
+    {
+        protected Guid Id { get; set; } = Guid.NewGuid();
+        public bool Active { get; set; } = true;
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+}
