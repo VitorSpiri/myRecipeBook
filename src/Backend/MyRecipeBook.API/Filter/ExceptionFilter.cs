36781edd6334863748345fe20022400bc4d9f9a5
Vitor Santos <vitorspiri1@gmail.com>
2025-04-06 11:08:13 -0300

    feat: ExceptionFilter

---
+++ src/Backend/MyRecipeBook.API/Filter/ExceptionFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyRecipeBook.Communication.Response;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+using System;
+using System.Net;
+
+namespace MyRecipeBook.API.Filter;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is MyRecipeBookException)
+            HandleProjectException(context);
+        else
+            ThrownUnkownException(context);
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidationException)
+        {
+            var exception = context.Exception as ErrorOnValidationException;
+
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Result = new BadRequestObjectResult(new ResponseErrorJson(exception.ErrorMessages));
+        }
+    }
+
+    private void ThrownUnkownException(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessagesException.UNKOWN_ERROR));
+    }
+}
+
