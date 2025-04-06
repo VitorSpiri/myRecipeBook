ea9dd092902f2dad8ebd3c7db490ae862fe217ea
Vitor Santos <vitorspiri1@gmail.com>
2025-04-06 11:09:24 -0300

    feat: Automapping class

---
+++ src/Backend/MyRecipeBook.Application/AutoMapper/AutoMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MyRecipeBook.Communication.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Application.AutoMapper
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping() 
+        {
+            RequestToDomain();
+        }
+
+        private void RequestToDomain()
+        {
+            CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
+                .ForMember(m => m.Password, opt => opt.Ignore());
+        }
+    }
+}
