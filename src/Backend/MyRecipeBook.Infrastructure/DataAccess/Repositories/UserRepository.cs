8b7e6192f5b9165bfa7874930bd454b3410bfa4f
Vitor Spiri <vitorspiri1@gmail.com>
2025-04-12 16:42:09 -0300

    feat: userRepository with Add and ExistsActiveUserWithEmail methods

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Repositories.User;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Repositories
+{
+    public class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepository
+    {
+        private readonly MyRecipeBookDbContext _dbContext;
+
+        public UserRepository(MyRecipeBookDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(User user) => await _dbContext.Users.AddAsync(user);
+        
+        public async Task<bool> ExistsActiveUserWithEmail(string email) => await _dbContext.Users.AnyAsync(u => u.Email == email && u.Active);
+  
+    }
+}
