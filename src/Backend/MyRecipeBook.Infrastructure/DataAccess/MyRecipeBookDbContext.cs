2f748838eccc00c6693bdf8606304b6758c59513
Vitor Spiri <vitorspiri1@gmail.com>
2025-04-12 16:41:18 -0300

    feat: adding dbContext

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/MyRecipeBookDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Infrastructure.DataAccess
+{
+    public class MyRecipeBookDbContext : DbContext
+    {
+        public MyRecipeBookDbContext(DbContextOptions<MyRecipeBookDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
