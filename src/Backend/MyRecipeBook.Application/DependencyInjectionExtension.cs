d21eebc6356d7f690324bd9564d3c9ee41ed1740
Vitor Santos <vitorspiri1@gmail.com>
2025-04-13 09:07:20 -0300

    feat: adding dependecy injection class

---
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Infrastructure.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Application
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddAplicationDependencyInjection(this IServiceCollection services)
+        {
+            AddRepositories(services);
+        }
+
+        public static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+            services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+        }
+    }
+}
